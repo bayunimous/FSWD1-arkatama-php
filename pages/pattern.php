d630e7e7b07bb41019200878adc2d0fd34446bc6
bayunimous <jigs4wcode@gmail.com>
2023-05-05 02:17:37 +0800

    [FIX]: Perubahan default tinggi dan default simbol, dan juga percobaan coding pernyataan argument pada tinggi dan simbol segitiga

--- pages/pattern.php
+++ pages/pattern.php
@@ -1,18 +1,18 @@
 <?php
 //fungsi upsideLeft
-function upsideLeft($tinggi_bintang)
+function upsideLeft($tinggi_bintang = 5, $simbol = '*')
 {
     echo "<p>Triangle Upside Left</p>";
     for ($a = $tinggi_bintang; $a > 0; $a--) {
         for ($b = $tinggi_bintang; $b >= $a; $b--) {
-            echo "*";
+            echo $simbol;
         }
         echo "<br>";
     }
 }
 
 //fungsi upsideRight
-function upsideRight($tinggi_bintang)
+function upsideRight($tinggi_bintang = 5, $simbol = '*')
 {
     echo "Triangle Upside Right<br><br>";
     for ($a = $tinggi_bintang; $a > 0; $a--) {
@@ -20,14 +20,27 @@ function upsideRight($tinggi_bintang)
             echo " &nbsp";
         }
         for ($a1 = $tinggi_bintang; $a1 >= $a; $a1--) {
-            echo "*";
+            echo $simbol;
+        }
+        echo "<br>";
+    }
+}
+
+//fungsi downsideLeft
+function downsideLeft($tinggi_bintang = 5, $simbol = '*')
+{
+    echo "<p>Triangle Downside Left</p>";
+
+    for ($a = 1; $a <= $tinggi_bintang; $a++) {
+        for ($c = $tinggi_bintang; $c >= $a; $c -= 1) {
+            echo $simbol;
         }
         echo "<br>";
     }
 }
 
 //fungsi downsideRight
-function downsideRight($tinggi_bintang)
+function downsideRight($tinggi_bintang = 5, $simbol = '*')
 {
     echo "<p>Triangle Downside Right</p>";
     for ($a = 1; $a <= $tinggi_bintang; $a++) {
@@ -35,63 +48,51 @@ function downsideRight($tinggi_bintang)
             echo " &nbsp";
         }
         for ($c = $tinggi_bintang; $c >= $a; $c -= 1) {
-            echo "*";
-        }
-        echo "<br>";
-    }
-}
-
-//fungsi downsideLeft
-function downsideLeft($tinggi_bintang)
-{
-    echo "<p>Triangle Downside Left</p>";
-
-    for ($a = 1; $a <= $tinggi_bintang; $a++) {
-        for ($c = $tinggi_bintang; $c >= $a; $c -= 1) {
-            echo " *";
+            echo $simbol;
         }
         echo "<br>";
     }
 }
 
 //fungsi conditionalStatement memilih pattern mana yang akan dipanggil
-function printPattern($patternName, $tinggi_bintang)
+function printPattern($patternName, $tinggi_bintang = 5, $simbol = '*')
 {
     switch ($patternName) {
         case "upsideLeft":
-            upsideLeft($tinggi_bintang);
+            upsideLeft($tinggi_bintang, $simbol);
             break;
         case "upsideRight":
-            upsideRight($tinggi_bintang);
-            break;
-        case "downsideRight":
-            downsideRight($tinggi_bintang);
+            upsideRight($tinggi_bintang, $simbol);
             break;
         case "downsideLeft":
-            downsideLeft($tinggi_bintang);
+            downsideLeft($tinggi_bintang, $simbol);
+            break;
+        case "downsideRight":
+            downsideRight($tinggi_bintang, $simbol);
             break;
         default:
-            echo "Invalid pattern name.";
+            echo "Salah memasukkan nama pattern";
     }
 }
 
 //memanggil fungsi upsideLeft, upsideRight, downsideLeft, downsideRight
-$tinggi_bintang = 5;
-upsideLeft($tinggi_bintang);
+/* 
+printPattern("upsideLeft");
 echo "<br>";
-upsideRight($tinggi_bintang);
+printPattern("upsideRight");
 echo "<br>";
-downsideLeft($tinggi_bintang);
+printPattern("downsideLeft");
 echo "<br>";
-downsideRight($tinggi_bintang);
+printPattern("downsideRight");
+*/
 
-//memanggil fungsi printPattern
-/* echo "<br>";
-printPattern("upsideLeft", $tinggi_bintang);
+
+
+printPattern("upsideLeft", 6, '$');
 echo "<br>";
-printPattern("upsideRight", $tinggi_bintang);
+printPattern("upsideRight", 9, '#');
 echo "<br>";
-printPattern("downsideLeft", $tinggi_bintang);
+printPattern("downsideLeft", 10, '@');
 echo "<br>";
-printPattern("downsideRight", $tinggi_bintang);
-echo "<br>"; */
+printPattern("downsideRight", 6,);
+echo "<br>";
