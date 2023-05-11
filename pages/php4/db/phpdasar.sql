b4e40e9e61f6e08fa91ff63ff5deffe25a1d70ff
bayunimous <jigs4wcode@gmail.com>
2023-05-11 15:54:25 +0800

    [FIX]: Perubahan isi database

--- pages/php4/db/phpdasar.sql
+++ pages/php4/db/phpdasar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 03 Apr 2021 pada 13.17
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 7.4.14
+-- Host: localhost:3306
+-- Generation Time: May 11, 2023 at 07:53 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,56 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
-  `nis` varchar(50) NOT NULL,
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `nama` varchar(255) NOT NULL,
   `tmpt_Lahir` varchar(50) NOT NULL,
   `tgl_Lahir` date NOT NULL,
-  `jekel` enum('Laki - Laki','Perempuan') NOT NULL,
-  `jurusan` enum('Teknik Jaringan Akses','Teknik Komputer dan Jaringan','Multimedia','Rekayasa Perangkat Lunak') NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('Admin','Staff') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `gambar` varchar(255) NOT NULL,
   `alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`nis`, `nama`, `tmpt_Lahir`, `tgl_Lahir`, `jekel`, `jurusan`, `email`, `gambar`, `alamat`) VALUES
-('210301', 'Tobiah Mc Mechan', 'Mangaldan', '2001-06-03', 'Perempuan', 'Teknik Komputer dan Jaringan', 'tmcmechan0@tamu.edu', '60433ff651b20.png', '14 Sloan Park'),
-('210302', 'Gav Colly', 'Kamieniec Podolski', '2003-01-23', 'Laki - Laki', 'Rekayasa Perangkat Lunak', 'gcolly1@simplemachines.org', '604340becfb49.png', '8 Brentwood Hill'),
-('210303', 'Editha Jiggens', 'Uyugan', '2000-04-22', 'Laki - Laki', 'Multimedia', 'ejiggens2@bloglines.com', '604342d27f34b.png', '2 Heffernan Street'),
-('210304', 'Celle Probert', 'Houston', '2003-07-05', 'Perempuan', 'Teknik Jaringan Akses', 'cprobert3@techcrunch.com', '604343274043b.png', '19387 Lake View Park'),
-('210305', 'Othella Pitts', 'Zhouzhuang', '2005-06-15', 'Perempuan', 'Multimedia', 'opitts4@etsy.com', '6043437e9dd54.png', '74105 Ryan Road'),
-('210306', 'Peta Layson', 'Oebai', '2002-06-02', 'Perempuan', 'Rekayasa Perangkat Lunak', 'playson5@gnu.org', '604343d4a6b79.png', '11 Hayes Terrace'),
-('210307', 'Sonny Adolfson', 'Lisui', '2001-02-08', 'Perempuan', 'Teknik Komputer dan Jaringan', 'sadolfson6@ed.gov', '6043441cd4d96.png', '5345 Oak Valley Center'),
-('210308', 'Dalt Polendine', 'Muang Sam Sip', '2003-02-08', 'Laki - Laki', 'Teknik Komputer dan Jaringan', 'dpolendine7@weebly.com', '60434474053e7.png', '8332 Nelson Alley'),
-('210309', 'Marlee Ertelt', 'Kapunduk', '2005-12-06', 'Laki - Laki', 'Multimedia', 'mertelt8@webnode.com', '604344cee38d6.png', '0330 Shasta Alley'),
-('210310', 'Haroun Mathiasen', 'Tambakbaya', '2000-01-21', 'Perempuan', 'Teknik Jaringan Akses', 'hmathiasen9@tuttocitta.it', '6043451f48ae3.png', '652 Heath Place'),
-('210311', 'Sylvan Mizzi', 'Kayes', '2000-10-14', 'Perempuan', 'Rekayasa Perangkat Lunak', 'smizzia@joomla.org', '6043456c9fc89.png', '87142 Scofield Drive'),
-('210312', 'Mirna Duffill', 'Patao', '2000-07-15', 'Perempuan', 'Teknik Komputer dan Jaringan', 'mduffill@pbs.org', '604345bd3abd1.png', '812 Hallows Parkway'),
-('210313', 'Sherlock Frackiewicz', 'Moate', '2004-01-31', 'Laki - Laki', 'Multimedia', 'sfrackiewiczc@uiuc.edu', '604346173ee94.png', '37998 Pankratz Alley'),
-('210314', 'Yolande Arne', 'Mpraeso', '2003-04-06', 'Perempuan', 'Teknik Jaringan Akses', 'yarned@youku.com', '6043464ca2b46.png', '2 Union Circle'),
-('210315', 'Catrina De Brett', 'Tasil', '2004-09-03', 'Laki - Laki', 'Teknik Jaringan Akses', 'cdebrette@xrea.com', '6043469155dc3.png', '8 Texas Court');
+INSERT INTO `siswa` (`id`, `nama`, `tmpt_Lahir`, `tgl_Lahir`, `email`, `role`, `gambar`, `alamat`) VALUES
+('001', 'John Doe', 'Banjarmasin', '2001-02-06', 'john.one@gmail.com', 'Admin', '645c9b301a10b.png', 'Indonesia'),
+('002', 'John Doe', 'Makassar', '1999-02-01', 'john.two@gmail.com', 'Staff', '645c9b6887d43.png', 'Indonesia');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
@@ -84,26 +70,26 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 --
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`nis`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
